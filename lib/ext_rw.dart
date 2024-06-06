cf2f25d284c6825cccdf739c0d78b2b5f8013e8b
a-givertzman <a.givertzman@icloud.com>
2024-06-06 17:20:35 +0300

    ExecutableQuery | added to export

--- lib/ext_rw.dart
+++ lib/ext_rw.dart
@@ -1,9 +1,12 @@
 library ext_rwcore;
 
 export 'src/api_client/address/api_address.dart';
+
 export 'src/api_client/query/api_query_type.dart';
 export 'src/api_client/query/sql_query.dart';
 export 'src/api_client/query/python_query.dart';
+export 'src/api_client/query/executable_query.dart';
+
 export 'src/api_client/reply/api_reply.dart';
 export 'src/api_client/request/api_request.dart';
 
@@ -20,6 +23,7 @@ export 'src/table_schema/table_schema_abstract.dart';
 export 'src/table_schema/data_schema.dart';
 export 'src/table_schema/schema_read.dart';
 export 'src/table_schema/schema_write.dart';
+
 export 'src/api_client/access/sql_read.dart';
 export 'src/api_client/access/sql_write.dart';
 export 'src/api_client/access/sql_builder.dart';
