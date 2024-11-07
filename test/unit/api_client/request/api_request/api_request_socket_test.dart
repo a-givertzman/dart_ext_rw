adcafcfd8726260dc295a0fcd6ce9e0e82d62f99
Anton Lobanov <a.givertzman@icloud.com>
2024-11-07 14:20:28 +0300

    Socket Message | Apply into ApiRequest

--- test/unit/api_client/request/api_request/api_request_socket_test.dart
+++ test/unit/api_client/request/api_request/api_request_socket_test.dart
@@ -2,8 +2,18 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:ext_rw/ext_rw.dart';
+import 'package:ext_rw/src/api_client/message/field_data.dart';
+import 'package:ext_rw/src/api_client/message/field_kind.dart';
+import 'package:ext_rw/src/api_client/message/field_size.dart';
+import 'package:ext_rw/src/api_client/message/field_syn.dart';
+import 'package:ext_rw/src/api_client/message/message_build.dart';
+import 'package:ext_rw/src/api_client/message/parse_data.dart';
+import 'package:ext_rw/src/api_client/message/parse_kind.dart';
+import 'package:ext_rw/src/api_client/message/parse_size.dart';
+import 'package:ext_rw/src/api_client/message/parse_syn.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hmi_core/hmi_core_result.dart';
+import 'package:hmi_core/hmi_core_option.dart';
 
 class FakeApiQueryType implements ApiQueryType {
   final bool _valid;
@@ -38,8 +48,23 @@ void main() {
       );
       // respond with received data
       serverSocket.listen((socket) {
-        socket.listen((data) {
-          socket.add(data);
+        socket.listen((bytes) {
+          final messageRcv = ParseData(
+            field: ParseSize(
+              size: FieldSize.def(),
+              field: ParseKind(
+                field: ParseSyn.def(),
+              ),
+            ),
+          );
+          final (kind, size, data) = messageRcv.parse(bytes).unwrap();
+          final messageSend = MessageBuild(
+            syn: FieldSyn.def(),
+            kind: FieldKind.string,
+            size: FieldSize.def(),
+            data: FieldData([]),
+          );
+          socket.add(messageSend.build(data));
         });
       });
       address = ApiAddress(
@@ -69,14 +94,13 @@ void main() {
           isA<Ok>(),
           reason: 'valid api request should return Ok as Result',
         );
-        if (result case Ok(value: final reply)) {
-          final replyAsJson = '{"authToken": "${reply.authToken}", "id": "${reply.id}", "query": "${reply.sql}", "data": ${json.encode(reply.data)}}';
-          expect(
-            replyAsJson,
-            query,
-            reason: 'data should be transferred correctly',
-          );
-        }
+        final reply = result.unwrap();
+        final replyAsJson = '{"authToken": "${reply.authToken}", "id": "${reply.id}", "query": "${reply.sql}", "data": ${json.encode(reply.data)}}';
+        expect(
+          replyAsJson,
+          query,
+          reason: 'data should be transferred correctly',
+        );
       }
     });
     ///
@@ -101,7 +125,7 @@ void main() {
         );
       }
     });
-
+    //
     tearDown(() async {
       serverSocket.close();
     });
