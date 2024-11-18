516f8d913b04dcf2206fe1cb01ba3ae7ce099441
Anton Lobanov <a.givertzman@icloud.com>
2024-11-18 16:53:27 +0300

    Socket Message | Message + test

--- lib/src/api_client/message/message.dart
+++ lib/src/api_client/message/message.dart
@@ -17,7 +17,29 @@ import 'package:ext_rw/src/api_client/message/parse_syn.dart';
 import 'package:hmi_core/hmi_core_log.dart';
 import 'package:hmi_core/hmi_core_option.dart';
 ///
-///
+/// Extracting `id`, `kind` and `payload` parts from the socket stream
+/// 
+/// Usage:
+/// 
+/// ```dart
+/// final message = Message(
+///   await Socket.connect(host, port),
+/// );
+/// message.stream.listen(
+///   (event) {
+///     final (FieldId id, FieldKind kind, Bytes bytes) = event;
+///     final text = String.fromCharCodes(bytes);
+///     print('onData | id: $id,  kind: $kind,  text: $text');
+///   },
+///   onError: (err) {
+///     print('onError | Error: $err');
+///     message.close();
+///   },
+///   onDone: () {
+///     print('onDone | Done');
+///     message.close();
+///   },
+/// );
 class Message {
   final _log = Log('Message');
   final StreamController<(FieldId, FieldKind, Bytes)> _controller = StreamController();
