cb403a4c2133b012d1d549e00715ff3158c73171
Anton Lobanov <a.givertzman@icloud.com>
2024-12-02 15:06:43 +0300

    TableSchema | Field | Builder

--- lib/src/table_schema/field.dart
+++ lib/src/table_schema/field.dart
@@ -1,14 +1,16 @@
 
 import 'package:ext_rw/src/table_schema/relation.dart';
+import 'package:flutter/widgets.dart';
 
 ///
 /// Replresentation settings for table column
-class Field {
+class Field<T> {
   final String _key;
   final String _title;
   final bool _hidden;
   final bool _edit;
   final Relation _relation;
+  final Widget Function(BuildContext, T)? _builder;
   ///
   /// **Represents table column settings**
   /// - [title] - display name of the column, if null, the [key] will be displayed
@@ -16,18 +18,21 @@ class Field {
   ///   - database column name if not relation
   ///   - relation key if this is relation column
   /// - [relation] - represents column, related from another table, where from data will be gotten
+  /// - [builder] - callback returns table column cell widget
   const Field({
     required String key,
     String? title,
     bool hidden = false,
     bool editable = false,
     Relation? relation,
+    Widget Function(BuildContext, T)? builder,
   }) :
     _key = key,
     _title = title ?? key,
     _hidden = hidden,
     _edit = editable,
-    _relation = relation ?? const Relation.empty();
+    _relation = relation ?? const Relation.empty(),
+    _builder = builder;
   ///
   ///
   String get key => _key;
@@ -43,6 +48,16 @@ class Field {
   ///
   ///
   Relation get relation => _relation;
+  ///
+  /// Returns cell widget build from specified [builder],  
+  /// Or by default retirns Text(value)
+  Widget build(BuildContext context, T value) {
+    final builder = _builder;
+    if (builder != null) {
+      return builder(context, value);
+    }
+    return Text('$value');
+  }
   //
   //
   @override
