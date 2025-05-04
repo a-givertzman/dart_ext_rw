a2ac7537fe1ca26e2d4c7f25c055ddfe2b19c1e7
Anton Lobanov <a.givertzman@icloud.com>
2025-05-04 19:26:42 +0300

    Field | update null

--- lib/src/table_schema/field_value.dart
+++ lib/src/table_schema/field_value.dart
@@ -55,25 +55,29 @@ class FieldValue<T> {
   bool update(T value) {
     if (_value != value) {
       _isChanged = true;
-      switch (type) {
-        case FieldType.bool:
-          // TODO Add type convertion
-          _value = value;
-          return true;
-        case FieldType.int:
-          // TODO Add type convertion
-          _value = value;
-          return true;
-        case FieldType.double:
-          // TODO Add type convertion
-          _value = value;
-          return true;
-        case FieldType.string:
-          _value = '$value' as T;
-          return true;
-        case FieldType.unknown:
-          _value = value;
-          return true;
+      if (value == null) {
+        _value = value;
+      } else {
+        switch (type) {
+          case FieldType.bool:
+            // TODO Add type convertion
+            _value = value;
+            return true;
+          case FieldType.int:
+            // TODO Add type convertion
+            _value = value;
+            return true;
+          case FieldType.double:
+            // TODO Add type convertion
+            _value = value;
+            return true;
+          case FieldType.string:
+            _value = '$value' as T;
+            return true;
+          case FieldType.unknown:
+            _value = value;
+            return true;
+        }
       }
     }
     return false;
